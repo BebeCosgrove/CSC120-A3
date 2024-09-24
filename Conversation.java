54dd203cd59f77000c9e176d887467d45479ebf0
BebeCosgrove <bcosgrove@smith.edu>
2024-09-24 16:44:55 -0400

    Submitting

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,131 @@
-class Conversation {
-
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * Conversation with Chatbot
+ * 
+ * @author Bebe Cosgrove
+ * @version September 2024
+ */
+public class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner rounds  = new Scanner(System.in);
+    System.out.print("How many rounds? ");
+  
+    /** User inputs number for number of rounds */ 
+    int numberofRounds = rounds.nextInt();
+    rounds.nextLine();
+    
+    /** Makes an array list for the transcript */ 
+    ArrayList <String> transcript = new ArrayList<String>();
+
+    transcript.add("TRANSCRIPT:");
+    String welcome_message = "Hi there!  What can I help you with?";
+    System.out.println(welcome_message);
+    transcript.add(welcome_message);
+
+    /** Array of pre-made phrases to respond to non-mirrored responses */  
+    String[] array = new String[]{"Interesting.", "Mhm.", "That's cool.", "Wow!"};
+
+
+    /** Loops through number of times user inputed */ 
+    for (int i = 0; i < numberofRounds; i++) {
+      String response = rounds.nextLine();
+
+      /** Adds response to transcript array */ 
+      transcript.add(response);
+
+      String[] wordsinResponse = response.split(" ");
+      /** boolean to set whether or not response has mirrored word in it */
+      boolean mirror = false; 
+
+      /** For loop to see if each word in response has a mirrored word */ 
+      for(int j = 0; j < wordsinResponse.length; j++){
+        if (wordsinResponse[j].equals("I") || wordsinResponse[j].equals("i")){
+          wordsinResponse[j] = "You";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("Am") || wordsinResponse[j].equals("am")){
+            wordsinResponse[j] = "are";
+            mirror = true;
+        }
+        else if (wordsinResponse[j].equals("Are") || wordsinResponse[j].equals("are")){
+          wordsinResponse[j] = "am";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("You") || wordsinResponse[j].equals("you")){
+          wordsinResponse[j] = "I";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("Me") || wordsinResponse[j].equals("me")){
+          wordsinResponse[j] = "you";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("My") || wordsinResponse[j].equals("my")){
+          wordsinResponse[j] = "your";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("Your") || wordsinResponse[j].equals("your")){
+          wordsinResponse[j] = "my";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("I'm") || wordsinResponse[j].equals("i'm")){
+          wordsinResponse[j] = "You're";
+          mirror = true;
+        }
+        else if (wordsinResponse[j].equals("You're") || wordsinResponse[j].equals("you're")){
+          wordsinResponse[j] = "I'm";
+          mirror = true;
+        }
+        
+    }
+
+    /** if response contains mirrored word, print the mirrored response */ 
+    if(mirror == true){
+       for(int u = 0; u < wordsinResponse.length; u++){
+        System.out.print(wordsinResponse[u] + " ");
+      
+      }
+      /** Joins together words in mirrored response */
+      String mirrored_response = String.join(" ", wordsinResponse);
+      
+      /** Adds mirrored response to transcript array */
+      transcript.add(mirrored_response + "?");
+
+      System.out.print("?");
+      System.out.println();
+
+    }
+    /** if response doesn't contain mirrored word, print a randomly chosen response from array of set responses */ 
+    else{
+      /** Picks random element from array of responses, figured this out using something I referenced in reflection */
+      int randomint = (int) Math.floor(Math.random()* array.length);
+      String random_response = array[randomint];
+      System.out.println(random_response);
+      transcript.add(random_response);
+    }
+
+    }
+    /** Prints out goodbye message */ 
+    String goodbye_message = "Bye!";
+    System.out.println(goodbye_message);
+    System.out.println("");
+    transcript.add(goodbye_message);
+
+    /** Prints out every element in transcript on seperate line, also figured this out using a source which I referenced in reflection */
+    transcript.forEach(System.out :: println);
+    
+
+    
+
+
+
+     
+    rounds.close();
+
+
+
+
   }
 }
+
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,28 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
+Nicole Choi, Rya Donelo
+
 - Which session(s) of TA / office hours did you attend?
+
+Sunday 9/22 1-3pm and Monday 9/23 7-9pm
+
+- Resources:
+I used this to figure out how to access a random element in an array.
+https://www.javatpoint.com/how-to-pick-random-elements-from-an-array
+
+I also used this for figuring out how to print out the elements of a list on seperate lines.
+https://stackoverflow.com/questions/12887857/display-each-list-element-in-a-separate-line-console 
+
 - What are your initial impressions of Java? 
+
+It's not as different from Python as I thought it was going to be. It wasn't horribly hard to adapt to after doing it for a little.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
+I can tell that most programming languages have similar elements like all have conditional statements and loops but there are different ways to do them in different languages.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+It was hard to figure out where to start and how to do the mirrored responses but TA hours really helped with that. I would tell someone to go to TA hours and talk about the assignment with the TA and your peers because it's a lot more helfpul than doing the assignment by yourself.
